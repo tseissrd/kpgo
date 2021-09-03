5f07af53d5507cc428ebc879cd5bd938013912a5
sovereign <allys@live.ru>
2021-09-03 18:08:26 +1000

    init

---
+++ src/main/java/com/springapp/kpgo/KpgoApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.kpgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpgoApplication.class, args);
+	}
+
+}
